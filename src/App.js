bfd93241d9e74a7cfdc603088ad192339fc23833
vishal rathor <rathorvishal975@gmail.com>
2023-03-23 16:58:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+
+    
+        <section class="ftco-section pt-5">
+		<div class="container">
+	
+			<div class="row justify-content-center">
+			    
+			  
+			    
+				<div class="col-md-7 col-lg-5">
+					<div class="login-wrap p-4 p-md-5">
+		      	<div class="d-flex align-items-center justify-content-center">
+		      		<img src='https://hospital.cfcsoftware.com/public/assets/img/images%20(4).jpeg' style={{height: '90px',borderRadius:"15px"}} />
+		      	</div>
+		      	 
+                    
+                    
+		      	<h3 class="text-center mb-4 text-primary"></h3>
+						<form method="POST" action="" id="singnupFrom" >
+                       
+		      		<div class="form-group">
+		      			<input type="text" class="form-control mt-2 p-2rounded-left" placeholder="" name="name" id="name" required/>
+		      			
+                            <span class="text-danger"></span>
+                      
+		      		</div>
+		      		<div class="form-group">
+		      			<input type="number" class="form-control mt-2 p-2rounded-left" placeholder="" name="mobile" id="mobile" required/>
+		      			
+                            <span class="text-danger"></span>
+                       
+		      		</div>
+		      		<div class="form-group">
+		      			<input type="email" class="form-control mt-2 p-2rounded-left" placeholder="" name="email" id="email" required/>
+		      		
+                            <span class="text-danger"></span>
+                        
+		      			
+		      		</div>
+	                <div class="form-group d-flex">
+	                  <input type="password" class="form-control mt-2 p-2rounded-left" placeholder="" name="password" id="password" required/>
+	                    
+                            <span class="text-danger"></span>
+                        
+	                </div>
+	                <div class="form-group d-flex">
+	                   <input type="password" class="form-control mt-2 p-2rounded-left" placeholder="" name="cpassword" id="cpassword" required/>
+	                 
+                            <span class="text-danger"></span>
+                      
+	                </div>
+	                <div  style={{marginTop: '7px' }} id="CheckPasswordMatch"></div>
+	            <div class="form-group">
+	            	<button type="submit" class="form-control btn btn-primary rounded submit px-3">Start your free trial</button>
+	            </div>
+	            
+	            <div class="w-100 form-group text-md-left text-white">
+	                       
+                            <a href="" class="text-warning">
+                               <p>Already Have an Account.</p><p>Login Here!</p>
+                            </a>
+
+                </div>
+               
+	          </form>
+	        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+    
+
+
+
     </div>
   );
 }
